f39f7513f25862b63b9c1e08010221a6a1ccd853
Hénoc <amaviganhenoc@gmail.com>
2025-05-22 10:50:56 +0000

    Ajout de la gestion de l'état de chargement dans le hook useAuth et amélioration de la logique de redirection dans le composant ProtectedRoute. Affichage d'un indicateur de chargement pendant la récupération des données utilisateur.

--- app/application/hooks/useAuth.ts
+++ app/application/hooks/useAuth.ts
@@ -7,24 +7,33 @@ import User from '../models/user';
 
 interface AuthContext {
     user: User | null;
+    loading: boolean;
 }
 
 export const useAuth = (): AuthContext => {
     const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const auth = getAuth();
         const unsubscribe = onAuthStateChanged(auth, async (firebaseUser: FirebaseUser | null) => {
-            if (firebaseUser) {
-                const userData = await userService.getUser(firebaseUser.uid);
-                setUser(userData);
-            } else {
+            try {
+                if (firebaseUser) {
+                    const userData = await userService.getUser(firebaseUser.uid);
+                    setUser(userData);
+                } else {
+                    setUser(null);
+                }
+            } catch (error) {
+                console.error("Erreur lors de la récupération des données utilisateur:", error);
                 setUser(null);
+            } finally {
+                setLoading(false);
             }
         });
 
         return () => unsubscribe();
     }, []);
 
-    return { user };
+    return { user, loading };
 };
--- app/components/ProtectedRoute.tsx
+++ app/components/ProtectedRoute.tsx
@@ -1,20 +1,36 @@
-// components/ProtectedRoute.tsx
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+"use client";
+
+import { useRouter } from "next/navigation";
 import { useAuth } from "../application/hooks/useAuth";
+import { useEffect } from "react";
 
 interface ProtectedRouteProps {
-  children?: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+  const router = useRouter();
 
-  if (!user || user.role !== "admin") {
-    return <Navigate to="/" />;
+  useEffect(() => {
+    if (!loading && (!user || user.role !== "admin")) {
+      router.push("/");
+    }
+  }, [user, loading, router]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    );
   }
 
-  return children ? <>{children}</> : <Outlet />;
+  if (!user || user.role !== "admin") {
+    return null;
+  }
+
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
