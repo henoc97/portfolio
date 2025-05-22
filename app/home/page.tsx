c27356f5f394339e93c2ee9d7bd653481bfe5f48
Hénoc <amaviganhenoc@gmail.com>
2025-05-22 10:20:10 +0000

    Ajout de la bibliothèque react-icons pour intégrer des icônes sociales dans le pied de page de la page d'accueil. Mise à jour de la structure du composant Home pour inclure des liens vers LinkedIn, GitHub, WhatsApp et Twitter, ainsi qu'un message de droits d'auteur.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,6 +7,7 @@ import Projects from "../components/sections/Projects";
 import Contact from "../components/sections/Contacts";
 import Layout from "../components/sections/layout";
 import Certificates from "../components/sections/Certificates";
+import { FaLinkedin, FaGithub, FaWhatsapp, FaTwitter } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -33,6 +34,47 @@ export default function Home() {
         <Blog />
         <Certificates />
         <Contact />
+        <footer className="bg-gray-900 text-white py-8">
+          <div className="container mx-auto px-4">
+            <div className="flex justify-center space-x-6">
+              <a
+                href="https://www.linkedin.com/in/henoc-amavigan/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-500 transition-colors"
+              >
+                <FaLinkedin size={30} />
+              </a>
+              <a
+                href="https://github.com/henock97"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-gray-400 transition-colors"
+              >
+                <FaGithub size={30} />
+              </a>
+              <a
+                href="https://wa.me/+22897045559"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-green-500 transition-colors"
+              >
+                <FaWhatsapp size={30} />
+              </a>
+              <a
+                href="https://twitter.com/votre-compte"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-blue-400 transition-colors"
+              >
+                <FaTwitter size={30} />
+              </a>
+            </div>
+            <div className="text-center mt-4">
+              <p>&copy; {new Date().getFullYear()} - Tous droits réservés</p>
+            </div>
+          </div>
+        </footer>
       </Layout>
     </div>
   );
